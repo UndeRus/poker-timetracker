b1aa0dee099b3c4e8322810c0100c61e675c3e5b
Kerrigan <xybermind@gmail.com>
2024-01-26 17:30:21 +0400

    rework commands

--- README.md
+++ README.md
@@ -7,15 +7,31 @@
 ### Usage:
 
 ```
-Usage: poker-tracker.exe [OPTIONS] --tasks <TASKS> --start-date <START_DATE> --end-date <END_DATE>
+Print result csv to terminal
+
+Usage: poker-tracker.exe print [OPTIONS] --tasks <TASKS> --start-date <START_DATE> --end-date <END_DATE>
 
 Options:
-  -t, --tasks <TASKS> # Filename to csv with all tasks
-  -s, --start-date <START_DATE> # Start date in format mm-dd-YYYY
-  -e, --end-date <END_DATE> # End date in format mm-dd-YYYY
-  --day-start <DAY_START>    [default: 10:00] # Workday start in format HH:MM
-  --day-end <DAY_END>        [default: 18:00] # Workday end in format HH:MM
+  -t, --tasks <TASKS>            Input csv file with tasks and priorities
+  -s, --start-date <START_DATE>  Start date in format mm-dd-YYYY
+  -e, --end-date <END_DATE>      Finish date in format mm-dd-YYYY
+      --ds <DAY_START>           Workday start time in format HH:MM [default: 10:00]
+      --de <DAY_END>             Workday end time in format HH:MM [default: 18:00]
   -h, --help                     Print help
+
+Export result csv to output file
+
+Usage: poker-tracker.exe export [OPTIONS] --tasks <TASKS> --start-date <START_DATE> --end-date <END_DATE> --output-filename <OUTPUT_FILENAME>
+
+Options:
+  -t, --tasks <TASKS>                      Input csv file with tasks and priorities
+  -s, --start-date <START_DATE>            Start date in format mm-dd-YYYY
+  -e, --end-date <END_DATE>                Finish date in format mm-dd-YYYY
+      --ds <DAY_START>                     Workday start time in format HH:MM [default: 10:00]
+      --de <DAY_END>                       Workday end time in format HH:MM [default: 18:00]
+  -o, --output-filename <OUTPUT_FILENAME>  Output csv filename
+  -h, --help                               Print help
+
 ```
 
 Examples of *.csv file with tasks is in resources/test directory
@@ -23,9 +39,11 @@ Examples of *.csv file with tasks is in resources/test directory
 
 ### TODO:
 
+- [X] Specify output to file/stdout
 - [ ] Specify complexity in planning poker cards
 - [ ] Tracking fill modes
 - [X] continuous
 - [ ] uniform
 - [ ] Schedule rules(or manual calendar)
 - [ ] Deadlines for specified tasks
+- [ ] Specify planning quants(hour/half-hour/etc)
--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,55 @@ pub mod poker_planner;
 mod prop_div;
 
 use chrono::{NaiveDate, NaiveTime, ParseError};
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
+use std::fs::File;
 use std::io;
+use std::io::Write;
 use std::path::PathBuf;
 
 #[derive(Parser)]
+#[command(name = "pt")]
+#[command(author = "Kerrigan <xybermind@gmail.com>")]
+#[command(about = "Simple time tracker calculator")]
+#[command(name = "pt", author, version, about)]
 struct Cli {
-    #[arg(short, long)]
+    #[command(subcommand)]
+    command: CliCommands,
+}
+
+#[derive(Subcommand)]
+enum CliCommands {
+    #[command(name = "print", about = "Print result csv to terminal")]
+    Print(CliArgs),
+    #[command(name = "export", about = "Export result csv to output file")]
+    Export(ExportArgs),
+}
+
+#[derive(Args)]
+struct CliArgs {
+    #[arg(short, long, help = "Input csv file with tasks and priorities")]
     tasks: PathBuf,
 
-    #[arg(short, long, value_parser = parse_date)]
+    #[arg(short, long, value_parser = parse_date, help = "Start date in format mm-dd-YYYY")]
     start_date: NaiveDate,
 
-    #[arg(short, long, value_parser = parse_date)]
+    #[arg(short, long, value_parser = parse_date, help = "Finish date in format mm-dd-YYYY")]
     end_date: NaiveDate,
 
-    #[arg(long, default_value = "10:00", value_parser = parse_time)]
+    #[arg(long = "ds", default_value = "10:00", value_parser = parse_time, help = "Workday start time in format HH:MM")]
     day_start: NaiveTime,
 
-    #[arg(long, default_value = "18:00", value_parser = parse_time)]
+    #[arg(long = "de", default_value = "18:00", value_parser = parse_time, help = "Workday end time in format HH:MM")]
     day_end: NaiveTime,
+}
+
+#[derive(Args)]
+struct ExportArgs {
+    #[clap(flatten)]
+    args: CliArgs,
+
+    #[arg(short, long, help = "Output csv filename")]
+    output_filename: PathBuf,
 }
 
 fn parse_date(arg: &str) -> Result<NaiveDate, ParseError> {
@@ -36,24 +65,44 @@ fn parse_time(arg: &str) -> Result<NaiveTime, ParseError> {
     NaiveTime::parse_from_str(arg, "%H:%M")
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let cli = Cli::parse();
-    run_pokerplanner(
-        cli.tasks,
-        cli.start_date,
-        cli.end_date,
-        cli.day_start,
-        cli.day_end,
-    );
+    match cli.command {
+        CliCommands::Print(args) => {
+            run_pokerplanner(
+                args.tasks,
+                args.start_date,
+                args.end_date,
+                args.day_start,
+                args.day_end,
+                io::stdout(),
+            );
+        }
+        CliCommands::Export(export_args) => {
+            let file = File::create(export_args.output_filename)?;
+            run_pokerplanner(
+                export_args.args.tasks,
+                export_args.args.start_date,
+                export_args.args.end_date,
+                export_args.args.day_start,
+                export_args.args.day_end,
+                file,
+            );
+        }
+    }
+    Ok(())
 }
 
-fn run_pokerplanner(
+fn run_pokerplanner<W>(
     tasks_file: PathBuf,
     date_from: NaiveDate,
     date_to: NaiveDate,
     workday_start: NaiveTime,
     workday_end: NaiveTime,
-) {
+    output: W,
+) where
+    W: Write + Sized,
+{
     let calendar = calendar::WorkCalendar::new();
     let tasks = csv_import::csv_import(tasks_file).unwrap();
     let time_records = poker_planner::calculate_hours_by_tasks(
@@ -64,7 +113,7 @@ fn run_pokerplanner(
         &workday_end,
         &calendar,
     );
-    csv_export::csv_export(io::stdout(), &time_records);
+    csv_export::csv_export(output, &time_records);
 }
 
 /*
